381c485ec37507824bd813f4021d4956b4f7bcd5
TuncayHp2 <tunc270@gmail.com>
2026-04-20 15:18:07 +0300

    up

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 // Node Utility Library
 // Buraya yeni fonksiyon/sınıfları ekleyin
 
+export { FiCopy } from "./FiFs/FiCopy.js";
 export { FiPropFile } from "./fiProps/FiPropFile.js";
 
 export const version = '0.0.1';
+
+
 
 
